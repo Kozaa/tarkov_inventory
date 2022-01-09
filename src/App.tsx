336e520fd211f871879c1f74d6a0fbf322d67861
KacperKozanecki <88181085+KacperKozanecki@users.noreply.github.com>
2022-01-09 17:53:20 +0100

    basic 3 section layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
-function App() {
+const Wrapper = styled.main`
+  width: 100%;
+  height: 100vh;
+  
+  display: flex;
+  
+  background-color: darkgrey;
+`
+
+const Section = styled.section`
+  height: 100%;
+  width: 100%;
+`
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Wrapper>
+        <Section>Loadout</Section>
+        <Section>Inventory</Section>
+        <Section>Stash</Section>
+      </Wrapper>
   );
 }
 
 export default App;
+
